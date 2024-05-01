93c5ed17e95b85937acf9a5e31284aa6a4657620
Yahiko <jhonbermudezpa98@gmail.com>
2024-05-01 10:25:44 -0500

    Presentation created

---
+++ src/layouts/Main.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+<template>
+    <main class="flex pt-20 px-48">
+        <div class="h-48 w-48 overflow-hidden rounded-full self-center">
+            <img src="/imgs/yahiko.jpg" alt="Descripción de la imagen" class="object-cover h-full w-full">
+        </div>
+        <article class="flex-1 ml-5 self-center text-justify">
+            <h1 class="text-2xl italic text-green-500">¡¿Q' hubo?! I'm just Yahiko.</h1>
+            <p>
+                Mi nombre real es Jhon Bermúdez, y soy Desarrollador Web Full-Stack.</p>
+            <p>Me gusta hacer las cosas sencillas (algo monocromático puede ser),
+                mi tiempo de ocio lo ocupo en su mayoría en el futbol, video juegos y relaciones con los más cercanos,
+                si estoy en un entorno de confianza suelo ser muy espontáneo y random, siento mucha empatía por la vida
+                en
+                general,
+                disfruto mucho los momentos con mis cercanos, pero me gusta en su mayoría estar solo.
+            </p>
+        </article>
+    </main>
+</template>
+<style scoped></style>
